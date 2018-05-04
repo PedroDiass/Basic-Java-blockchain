a6ebfcc4383d9a43f06792fa0b1d417e2e1eb39f
Suvh <38983837+Suvh@users.noreply.github.com>
2018-05-04 11:41:18 -0300

    Add files via upload

---
+++ Bloco.java
@@ -0,0 +1,146 @@
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.time.LocalDate;
+
+public class Bloco {
+	private String hash;						//hash do bloco.
+	private String hashAnterior;				//hash do bloco anterior.
+	private String conteudo;					//conteudo do bloco ( como String para facilitar ).
+	private LocalDate data;						//data de criacao do bloco.
+	private long nanoTempo;						//tempo de criacao do bloco em nanosegundos.
+	
+	private static final int dificuldade = 4;	//dificuldade de criacao dos blocos ( numero de zeros encontrados 
+	
+	//Construtor privado para só ser possivel criar um bloco a partir da mineracao.
+	private Bloco(String conteudo, String hashAnterior, String hash, long nanoTempo) {
+		this.conteudo = conteudo;
+		this.hashAnterior = hashAnterior;
+		this.nanoTempo = nanoTempo;
+		this.data = LocalDate.now();
+		
+		this.hash = hash;
+	}
+	
+	//Metodo para minerar um novo bloco. ( DUUH. )
+	public static Bloco minerarBloco(String conteudo, String hashAnterior) throws IOException {
+		TextFile tf = new TextFile("Blockchain", "C:\\Files Blockchain");
+		String hash = "                                                                 ";
+		int auxContador = 0;
+		long auxNano = 0;
+		
+		//Proof of work (prova de trabalho)  
+		while(!hash.startsWith(gerarStringDificuldade())) {
+			auxNano = System.nanoTime();
+			hash = geradorHash(conteudo + hashAnterior + auxNano);
+			System.out.println(hash);
+			auxContador++;
+		}
+		
+		Bloco bloco = new Bloco(conteudo, hashAnterior, hash, auxNano);
+		tf.addLinha(bloco.toString() + " Quantidade de Hashs Gerados = " + auxContador);
+		tf.SalvarFile();
+		return bloco;
+	}
+	
+	//Metodo utilizado para retornar o Hash de cada bloco ( Funcao Hash Sha-256 )
+	public static String geradorHash(String entrada){		
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");	        
+			byte[] hash = digest.digest(entrada.getBytes("UTF-8"));	        
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}	
+	
+	//Concatena uma String com o mesmo numero de zeros da dificuldade.
+	private static String gerarStringDificuldade() {
+		String aux = "";
+		for(int i = 0; i < dificuldade; i++) {
+			aux += 0;
+		}
+		return aux;
+	}
+	
+	public String getHash() {
+		return this.hash;
+	}
+	
+	public LocalDate getData() {
+		return data;
+	}
+	
+	public String getConteudo() {
+		return conteudo;
+	}
+	
+	public void setConteudo(String conteudoAdulterado) {
+		this.conteudo = conteudoAdulterado;
+		this.hash = this.geradorHash(conteudoAdulterado + this.hashAnterior + this.nanoTempo);
+	}
+	
+	//Altera o conteudo do bloco e refaz a prova de trabalho para q o novo hash seja valido.
+	public void setConteudoRefazendoHash(String conteudoAdulterado) throws IOException {
+		TextFile tf = new TextFile("Blockchain", "C:\\Files Blockchain");
+		this.conteudo = conteudoAdulterado;
+		String hash = "                          ";
+		long auxNano = 0;
+		int contador = 0;
+		while(!hash.startsWith(this.gerarStringDificuldade())) {
+			auxNano = System.nanoTime();
+			hash = geradorHash(conteudo + hashAnterior + auxNano);
+			System.out.println(hash);
+			contador++;
+		}
+		this.nanoTempo = auxNano;
+		this.hash = hash;
+		tf.addLinha(this.toString() + " Contador ==> " + contador);
+		tf.SalvarFile();
+	}
+	
+	//Altera o hash anterior do bloco e refaz a prova de trabalho para q o novo hash seja valido.
+	public void setHashAnterior(String hashAnterior) throws IOException {
+		TextFile tf = new TextFile("Blockchain", "C:\\Files Blockchain");
+		this.hashAnterior = hashAnterior;
+		String hash = "                          ";
+		long auxNano = 0;
+		int contador = 0;
+		while(!hash.startsWith(this.gerarStringDificuldade())) {
+			auxNano = System.nanoTime();
+			hash = geradorHash(conteudo + hashAnterior + auxNano);
+			System.out.println(hash);
+			contador++;
+		}
+		this.nanoTempo = auxNano;
+		this.hash = hash;
+		tf.addLinha(this.toString() + " Contador ==> " + contador);
+		tf.SalvarFile();
+		
+//		this.hashAnterior = hashAnterior;
+//		
+//		Bloco.minerarBloco(this.conteudo, hashAnterior);
+	}
+	
+	public String getHashAnterior() {
+		return this.hashAnterior;
+	}
+
+	public long getNanoTempo() {
+		return nanoTempo;
+	}
+
+	@Override
+	public String toString() {
+		return "Bloco [hash=" + hash + ", hashAnterior=" + hashAnterior + ", conteudo=" + conteudo + ", data=" + data
+				+ ", nanoTempo=" + nanoTempo + "]";
+	}
+	
+	
+}
---
+++ CadeiaBlocos.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CadeiaBlocos {
+	public static void main(String[] args) throws IOException {
+		ArrayList<Bloco> cadeia = new ArrayList<>(6);
+		
+		// Minera 6 blocos e os adiciona a cadeia de blocos.
+		cadeia.add(Bloco.minerarBloco("Olá, eu sou o bloco genesis", "0"));
+		cadeia.add(Bloco.minerarBloco("Olá, eu sou o segundo bloco da cadeia.", cadeia.get(cadeia.size() - 1).getHash()));
+		cadeia.add(Bloco.minerarBloco("E eu sou o terceiro bloco :)", cadeia.get(cadeia.size() - 1).getHash()));
+		cadeia.add(Bloco.minerarBloco("Chega de blocos!!", cadeia.get(cadeia.size() - 1).getHash()));
+		cadeia.add(Bloco.minerarBloco("Só mais dois plz!!", cadeia.get(cadeia.size() - 1).getHash()));
+		cadeia.add(Bloco.minerarBloco("Pronto, agr acabou!!", cadeia.get(cadeia.size() - 1).getHash()));
+		
+		// Usei apenas para separar a execucao do programa.
+		Scanner in = new Scanner(System.in);
+		in.nextLine();
+		
+		// Corrompe a cadeia de blocos e depois faz alguns testes para verificar o estado da cadeia.
+		CorromperCadeia(1, cadeia);
+		System.out.println(cadeia.get(1).getConteudo());
+		System.out.println(ValidarCadeia(cadeia));
+	}
+	
+	// Metodo de validacao da cadeia de blocos, retorna true se a cadeia estiver valida e false se a cadeia estiver corrompida.
+	static boolean ValidarCadeia(ArrayList<Bloco> cadeia) {
+		
+		// Verifica se todos os blocos da cadeia possuem o proof of work ( prova de trabalho ).
+		for(int i = 0; i < cadeia.size(); i++) {
+			if(!cadeia.get(i).getHash().substring(0, 4).equals("0000")) {
+				return false;
+			}
+		}
+		
+		//Verifica se os hashs de cada bloco estao de acordo com as suas informacoes
+		for(int i = 0; i < cadeia.size(); i++) {
+			if(!cadeia.get(i).geradorHash(cadeia.get(i).getConteudo() + cadeia.get(i).getHashAnterior() + cadeia.get(i).getNanoTempo()).equals(cadeia.get(i).getHash())) {
+				return false;
+			}
+		}
+		
+		// Verifica se a conexao entre os blocos da cadeia está valida.
+		for(int i = 1; i < cadeia.size(); i++) {
+			if(!cadeia.get(i).getHashAnterior().equals(cadeia.get(i - 1).getHash())) {
+				System.out.println("Erro de validação, o bloco " + (i - 1) + " foi adulterado.");
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	// Tenta corromper a cadeia de blocos refazendo todas as provas de trabalho.
+	static void CorromperCadeia(int index, ArrayList<Bloco> cadeia) throws IOException {
+		cadeia.get(index).setConteudoRefazendoHash("Muahahaha, alterei sua cadeia de blocos");
+		int aux = index;
+		for(int i = ++index; i < cadeia.size(); i++) {
+			cadeia.get(i).setHashAnterior(cadeia.get(aux++).getHash());
+		}
+	}
+}
